9400b407449d938c9db612c284b428561e43104d
Yonas Gelgelo <gelgeloyonas@gmail.com>
2025-08-30 20:38:52 +0300

    Create Specific Components in a React Application

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,6 +1,6 @@
-import Header from './Header';
-import MainContent from './MainContent';
-import Footer from './Footer';
+import Header from './Component/Header';
+import MainContent from './Component/MainContent';
+import Footer from './Component/Footer';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
