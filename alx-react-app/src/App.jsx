ab048a9e749cdd1cee563d2852a88622796be664
Yonas Gelgelo <gelgeloyonas@gmail.com>
2025-08-31 17:01:04 +0300

    3. Create a User Profile Card Using Props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import WelcomeMessage from './components/WelcomeMessage';
+import UserProfile from './components/UserProfile';
 
 function App() {
   return (
@@ -10,6 +11,11 @@ function App() {
       <MainContent />
       <Footer />
       <WelcomeMessage />
+      <UserProfile 
+        name="Alice" 
+        age="25" 
+        bio="Loves hiking and photography" 
+      />
     </>
   );
 }
