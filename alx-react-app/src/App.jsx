8c984811437d0696c857fd8ff6f35f65ee347857
Yonas Gelgelo <gelgeloyonas@gmail.com>
2025-08-31 16:31:08 +0300

    add welcomemassage

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,7 +1,7 @@
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
-import WelcomeMessage from './WelcomeMessage';
+import WelcomeMessage from './components/WelcomeMessage';
 
 function App() {
   return (
