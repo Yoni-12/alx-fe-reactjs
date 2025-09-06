e7bb5ae00035bee68d4b80bf25e5ef89cca8d1f1
Yonas Gelgelo <gelgeloyonas@gmail.com>
2025-09-06 20:36:24 +0300

    Checks for the use of context on the

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,12 +1,12 @@
-import ProfilePage from "./components/ProfilePage";
-import UserContext from "./components/UserContext";
+import UserProfile from "./components/UserProfile";
+import UserContext from "./UserContext";
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
     <UserContext.Provider value={userData}>
-      <ProfilePage />
+      <UserProfile />
     </UserContext.Provider>
   );
 }
